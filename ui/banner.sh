2bcf782bd816f94cbf06fd07d20165a27c700267
chamroosh <monstersec98@gmail.com>
2026-08-01 17:53:08 +0330

    [fix] prerelease, release, [up] banner

--- ui/banner.sh
+++ ui/banner.sh
@@ -4,19 +4,19 @@ show_banner()
 {
     W_LOGO="${BOLD}${WHITE}"
     R_LOGO="${BOLD}${RED}"
-    VERSION="v2.1.0-beta" # یا متغیر پروژه شما $DAYPASS_VERSION
+    VERSION="v2.1.0-beta" 
 
     echo
 
-    printf "${W_LOGO}          .=:   :-+++=-.      ${CYAN}____              ____${RESET}\n"
-    printf "${W_LOGO}      .-+*##- :*##+==*##=     ${CYAN}|  _ \  __ _ _   _|  _ \  __ _ ___ ___${RESET}\n"
-    printf "${W_LOGO}      =#*###:.##+     =*#-    ${CYAN}| | | |/ _\` | | | | |_) / _\` / __/ __|${RESET}\n"
-    printf "${W_LOGO}      .  **#: ***:   .+#*.    ${CYAN}| |_| | (_| | |_| |  __/ (_| \__ \__ \\\\${RESET}\n"
-    printf "${R_LOGO}        .**#: .+***++*+=.     ${CYAN}|____/ \__,_|\__, |_|   \__,_|___/___/${RESET}\n"
-    printf "${R_LOGO}        .***:.+**+=-::::-:    ${CYAN}             |___/${RESET}\n"
-    printf "${R_LOGO}        .***:=+=:      :--.   ${RESET}\n"
-    printf "${R_LOGO}        .***::-:       :--.   ${WHITE}🐱 github.com/Chamroosh98${RESET}\n"
-    printf "${R_LOGO}        .++*. :--:....:--:    ${WHITE}🏷️ Version     : %s${RESET}\n" "$VERSION"
+    printf "${W_LOGO}          .=:   :-+++=-.${RESET}\n"
+    printf "${W_LOGO}      .-+*##- :*##+==*##=${RESET}\n"
+    printf "${W_LOGO}      =#*###:.##+     =*#-    ${RED}____              ${GRAY}____${YELLOW} %s${RESET}\n" "$VERSION"
+    printf "${W_LOGO}      .  **#: ***:   .+#*.    ${RED}|  _ \  __ _ _   _${GRAY}|  _ \  __ _ ___ ___${RESET}\n"
+    printf "${R_LOGO}        .**#: .+***++*+=.     ${RED}| | | |/ _\` | | | |${GRAY}| |_) / _\` / __/ __|${RESET}\n"
+    printf "${R_LOGO}        .***:.+**+=-::::-:    ${RED}| |_| | (_| | |_| |${GRAY}|  __/ (_| \__ \__ \\\\${RESET}\n"
+    printf "${R_LOGO}        .***:=+=:      :--.   ${RED}|____/ \__,_|\__, |${GRAY}|_|   \__,_|___/___/${RESET}\n"
+    printf "${R_LOGO}        .***::-:       :--.   ${RED}             |___/${RESET}\n"
+    printf "${R_LOGO}        .++*. :--:....:--:    ${WHITE}🐱 github.com/Chamroosh98${RESET}\n"
     printf "${R_LOGO}        .+++:  .::::::--:     ${RESET}\n"
     printf "${R_LOGO}       =+++++=     .:::.      ${RESET}\n"
     printf "${R_LOGO}       .......  .::::.        ${RESET}\n"
