5e09d292a00ea614116eeacb468d814cdd399939
chamroosh <monstersec98@gmail.com>
2026-08-10 06:20:10 +0330

    [del]

--- test/qemu_init.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo pacman -S qemu-base qemu-system-x86 qemu-system-arm qemu-system-mips qemu-img
-
-# Debian/Ubuntu base -> sudo apt install qemu-base qemu-system-x86 qemu-system-arm qemu-system-mips qemu-img
