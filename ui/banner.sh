b8ff94f9931d0a3e85123ebf22ce68e2921a2499
chamroosh <monstersec98@gmail.com>
2026-08-26 18:19:21 +0330

    [up] template.go

--- ui/banner.sh
+++ ui/banner.sh
@@ -2,20 +2,19 @@
 
 show_banner()
 {
-
     VERSION="v1.2.0"
-    GITHUB="github.com/Chamroosh98"
+    GITHUB="github.com/Chamroosh98/DayPass"
 
     echo
-    printf "  %b ____              %b____%b                     %b%s%b\n" "${BOLD}${RED}" "${BOLD}${WHITE}" "${RESET}" "${GRAY}" "$VERSION" "${RESET}"
-    printf "  %b|  _ \\  __ _ _   _%b|  _ \\  __ _ ___ ___%b\n" "${BOLD}${RED}" "${BOLD}${WHITE}" "${RESET}"
-    printf "  %b| | | |/ _\` | | | |%b| |_) / _\` / __/ __|%b\n" "${BOLD}${RED}" "${BOLD}${WHITE}" "${RESET}"
-    printf "  %b| |_| | (_| | |_| |%b|  __/ (_| \\__ \\__ \\%b\n" "${BOLD}${RED}" "${BOLD}${WHITE}" "${RESET}"
-    printf "  %b|____/ \\__,_|\\__, |%b|_|   \\__,_|___/___/%b\n" "${BOLD}${RED}" "${BOLD}${WHITE}" "${RESET}"
-    printf "  %b             |___/%b\n" "${BOLD}${RED}" "${RESET}"
+
+    echo "${BOLD}${RED} ____              ${BOLD}${WHITE}____${RESET}              "
+    echo "${BOLD}${RED}|  _ \  __ _ _   _${BOLD}${WHITE}|  _ \  __ _ ___ ___${RESET}"
+    echo "${BOLD}${RED}| | | |/ _\` | | | |${BOLD}${WHITE}| |_) / _\` / __/ __|${RESET}   ${GRAY}${VERSION}${RESET}"
+    echo "${BOLD}${RED}| |_| | (_| | |_| |${BOLD}${WHITE}|  __/ (_| \__ \__ \\${RESET}   ${WHITE}${GITHUB}${RESET}"
+    echo "${BOLD}${RED}|____/ \__,_|\__, |${BOLD}${WHITE}|_|   \__,_|___/___/${RESET}"
+    echo "${BOLD}${RED}             |___/${RESET}"
+
     echo
-    printf "  %b%s%b\n" "${CYAN}" "${GITHUB}" "${RESET}"
-    echo
-    printf "  %b───────────────────── 🕊️ Remembering the IRAN Massacre on Jan 8-9, 2026 ─────────────────────%b\n" "${GRAY}" "${RESET}"
+    echo "${GRAY}───────────────────── 🕊️ Remembering the IRAN Massacre on Jan 8-9, 2026 ─────────────────────${RESET}"
     echo
 }
